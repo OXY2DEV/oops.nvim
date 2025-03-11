36d7f99bc3b1f2112232001516b8239a2a02ffcb
Shawon <mdmoinulhossainshawon@gmail.com>
2025-03-12 04:20:37 +0600

    refactor: Seperated rules from code

--- lua/oops.lua
+++ lua/oops.lua
@@ -4,56 +4,52 @@ local oops = {};
 ---
 oops.config = {
 	force = false,
-	rules = {
-		W_to_w = {
-			condition = function (cmd)
-				return string.match(cmd, "^W") ~= nil;
-			end,
+	ignore_types = { "input", "expr" },
 
-			fix = function ()
-				return "w";
-			end
-		},
-		Q_to_q = {
-			condition = function (cmd)
-				return string.match(cmd, "^Q") ~= nil;
-			end,
-
-			fix = function ()
-				return "q";
-			end
-		},
-	}
+	rules = require("oops.rules")
 };
 
-oops.__can_fix = function (cmd)
-	for name, rule in pairs(oops.config.rules) do
-		local _, condition = pcall(rule.condition, cmd);
+oops.__get_prompt = function (kind)
+	if kind == "cmd" then
+		return ":";
+	elseif kind == "search_down" then
+		return "?";
+	elseif kind == "search_up" then
+		return "?";
+	elseif kind == "expr" then
+		return "=";
+	elseif kind == "input" then
+		return "@";
+	end
+end
+
+oops.__can_fix = function (kind, cmd)
+	local keys = vim.tbl_keys(oops.config.rules);
+	table.sort(keys);
+
+	for _, key in ipairs(keys) do
+		local rule = oops.config.rules[key] or {};
+		local _, condition = pcall(rule.condition, kind, cmd);
 
 		if condition == true then
-			return name, rule;
+			return key, rule;
 		end
 	end
 end
 
-oops.__run = function (mode, before, cmd)
-	local prompt = "";
-
-	if mode == "cmd" then
-		prompt = ":";
-	elseif mode == "search" then
-		prompt = "/";
-	elseif mode == "expr" then
-		prompt = "=";
+oops.__run = function (old_prompt, old_command, new_prompt, new_command, force)
+	if force == true then
+		vim.api.nvim_exec2(new_prompt .. new_command, {});
+		return;
 	end
 
 	vim.ui.select({ "Y", "N" }, {
 		prompt = "Run Fixup command?",
-		default = "y",
+		default = "Y",
 
 		format_item = function (item)
 			if item == "Y" then
-				return string.format("Run fixup(%s%s -> %s%s)", prompt, before, prompt, cmd);
+				return string.format("Run fixup(%s%s -> %s%s)", old_prompt, old_command, new_prompt, new_command);
 			else
 				return "Do nothing."
 			end
@@ -62,35 +58,59 @@ oops.__run = function (mode, before, cmd)
 		if result ~= "Y" then
 			return;
 		else
-			vim.api.nvim_exec2(prompt .. cmd, {});
+			vim.api.nvim_exec2(new_prompt .. new_command, {});
 		end
 	end)
 end
 
-oops.fix = function (mode)
-	mode = mode or "cmd";
-	local last_command = vim.fn.histget(mode, -1);
+oops.fix = function (kind, force)
+	kind = kind or vim.g.__last_cmdtype or "cmd";
+	force = force or oops.config.force or false;
+
+	if oops.config.ignore_types and vim.list_contains(oops.config.ignore_types, kind) then
+		return;
+	end
+
+	local last_command;
+
+	if kind == "search_down" or kind == "search_up" then
+		last_command = vim.fn.histget("search", -1);
+	else
+		last_command = vim.fn.histget(kind, -1);
+	end
 
 	if last_command == "" then
-		--- No last command!
 		return;
-	end
-
-	local name, rule = oops.__can_fix(last_command or "");
-
-	if name == nil then
-		return;
-	end
-
-	local can_fix, fixed = pcall(rule.fix, last_command);
-
-	if can_fix == true and type(fixed) == "string" then
-		oops.__run(mode, last_command, fixed);
 	else
-		vim.api.nvim_echo({
-			{ "󰘳 Oops.nvim", "DiagosticVirtTextOk" },
-			{ ": Failed to run rule " .. name, "Comment" }
-		})
+		local name, rule = oops.__can_fix(kind, last_command);
+
+		if name == nil then
+			return;
+		end
+
+		local can_fix, new_prompt, new_command = pcall(
+			rule.fix,
+
+			oops.__get_prompt(kind),
+			last_command
+		);
+
+		if can_fix == true then
+			oops.__run(
+				oops.__get_prompt(kind),
+				last_command,
+
+				new_prompt,
+				new_command,
+
+				force
+			);
+		else
+			vim.api.nvim_echo({
+				{ "󰘳 Oops.nvim", "DiagosticVirtTextOk" },
+				{ ": Failed to run rule " .. name, "Comment" }
+			})
+		end
 	end
 end
 
---
+++ lua/oops/rules.lua
@@ -0,0 +1,139 @@
+--- Rules for oops.nvim
+local rules = {}
+
+--- :W → :w, :Q → :q, :Wq → :wq
+rules.caps_locked = {
+	condition = function (prompt, command)
+		if prompt ~= "cmd" then return; end
+
+		local patterns = {
+			"^W", "^Q", "^E",
+			"^[wW][qQ]", "^[qQ][aA]",
+
+			"^[wW][qQ][aA]", "^[wW][qQ][aA][lL][lL]"
+		}
+
+		for _, pattern in ipairs(patterns) do
+			if string.match(command, pattern) then
+				return true;
+			end
+		end
+
+		return false;
+	end,
+
+	fix = function (prompt, command)
+		return prompt, string.gsub(command, "^%w+", string.lower);
+	end
+};
+
+--- :q' → :q, :q` → :q
+rules.q_quote = {
+	condition = function (prompt, command)
+		if prompt ~= "cmd" then return; end
+
+		return string.match(command, "^q[`'\"]") ~= nil;
+	end,
+
+	fix = function (prompt, command)
+		return prompt, string.gsub(command, "^q[`'\"]", "q")
+	end
+};
+
+--- :qw → :wq
+rules.reversed_wq = {
+	condition = function (prompt, command)
+		if prompt ~= "cmd" then return; end
+
+		return string.match(command, "^[qQ][wW]") ~= nil;
+	end,
+
+	fix = function (prompt, command)
+		return prompt, string.gsub(command, "^[qQ][wW]", "wq")
+	end
+};
+
+--- :!q → :q!
+rules.accidental_bash = {
+	condition = function (prompt, command)
+		if prompt ~= "cmd" then return; end
+
+		return string.match(command, "^![wWqQeE]+") ~= nil;
+	end,
+
+	fix = function (prompt, command)
+		local removed_excl = string.gsub(command, "^!", "");
+
+		return prompt, string.gsub(removed_excl, "^[wWqQeE]+", function (val)
+			return string.lower(val) .. "!";
+		end);
+	end
+};
+
+--- :Set opt → :set opt
+rules.set_with_S = {
+	condition = function (prompt, command)
+		if prompt ~= "cmd" then return; end
+
+		return string.match(command, "^Set") ~= nil;
+	end,
+
+	fix = function (prompt, command)
+		return prompt, string.gsub(command, "^Set%w*", string.lower);
+	end
+};
+
+--- :luabprint("hi") → :lua print("hi")
+rules.lua_with_b = {
+	condition = function (prompt, command)
+		if prompt ~= "cmd" then return; end
+
+		return string.match(command, "^luab") ~= nil;
+	end,
+
+	fix = function (prompt, command)
+		return prompt, string.gsub(command, "^luab", "lua");
+	end
+};
+
+
+--- :moh → :noh
+rules.moh = {
+	condition = function (prompt, command)
+		if prompt ~= "cmd" then return; end
+
+		return string.match(command, "^moh") ~= nil;
+	end,
+
+	fix = function (prompt, command)
+		return prompt, string.gsub(command, "^m", "n");
+	end
+};
+
+--- :sfoo/bar → :s/foo/bar
+rules.sub_wo_slash = {
+	condition = function (prompt, command)
+		if prompt ~= "cmd" then return; end
+
+		return string.match(command, "^s[^/]") ~= nil;
+	end,
+
+	fix = function (prompt, command)
+		return prompt, string.gsub(command, "^s[^/]", "s/");
+	end
+};
+
+--- :'<,'>sfoo/bar → :'<,'>s/foo/bar
+rules.vsub_wo_slash = {
+	condition = function (prompt, command)
+		if prompt ~= "cmd" then return; end
+
+		return string.match(command, "'<,'>^s[^/]") ~= nil;
+	end,
+
+	fix = function (prompt, command)
+		return prompt, string.gsub(command, "'<,'>^s[^/]", "s/");
+	end
+};
+
+return rules;
