4370723c5ac8788cf4515a1a39b23773fd50cd20
Shawon <mdmoinulhossainshawon@gmail.com>
2025-03-12 04:20:11 +0600

    feat: Last command type is now stored

--- plugin/oops.lua
+++ plugin/oops.lua
@@ -1,3 +1,24 @@
+vim.g.__last_cmdtype = "cmd";
+
 vim.api.nvim_create_user_command("Oops", function (data)
-	require("oops").fix();
+	require("oops").fix(vim.g.__last_cmdtype);
 end, {})
+
+vim.api.nvim_create_autocmd("CmdlineEnter", {
+	callback = function (event)
+		local cmd = event.match;
+
+		if cmd == ":" then
+			vim.g.__last_cmdtype = "cmd";
+		elseif cmd == "?" then
+			vim.g.__last_cmdtype = "search_down";
+		elseif cmd == "/" then
+			vim.g.__last_cmdtype = "search_up";
+		elseif cmd == "=" then
+			vim.g.__last_cmdtype = "expr";
+		elseif cmd == "@" then
+			vim.g.__last_cmdtype = "input";
+		end
+	end
+})
+
